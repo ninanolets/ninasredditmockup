df54768df12103f603cb0bc83c03ea644c6f2e5b
ninanolets <marinaa.noleto@gmail.com>
2020-09-16 19:06:31 +0200

    Fix static

--- subreddits/models.py
+++ subreddits/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 class Subreddit(models.Model):
-    name = models.CharField(max_length=40)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    slug = models.CharField(max_length=40)
     title = models.CharField(max_length=80)
     description = models.TextField(blank=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+
+    def __str__(self):
+        return self.slug
